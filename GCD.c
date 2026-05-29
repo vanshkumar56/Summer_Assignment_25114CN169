49cbf22be22ed896939e09998fe7621aaa82aff7
Vansh Kumar <vanshkumar8810826073@gmail.com>
2026-05-29 16:27:09 +0530

    Third Program Done

---
+++ GCD.c
@@ -0,0 +1,19 @@
+// Write a program to Find GCD of two numbers.
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("GCD is: %d\n", a);
+
+    return 0;
+}
