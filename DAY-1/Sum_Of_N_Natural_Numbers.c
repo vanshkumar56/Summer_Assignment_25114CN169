33adf669765cabc8d1d9022544c9b3fdc33817d3
Vansh Kumar <vanshkumar8810826073@gmail.com>
2026-06-01 11:57:47 +0530

    Initial commit

---
+++ DAY-1/Sum_Of_N_Natural_Numbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    int n,N;
+printf("Enter Starting Number: ");
+scanf( "%d" ,&n);
+printf("Enter Ending Number: ");
+scanf( "%d" ,&N);
+if (n>N)
+{
+    printf("The Starting Number Is Greater Than The Ending Number So Sum Is Not Possible");
+}
+else if(n==N){
+    printf("The Sum Is %d", n);
+}
+else{
+    int sum=0;
+for (int i = n; i <= N; i++)
+{
+    sum=sum+i;
+}
+printf("The Value After Addition is %d", sum);
+}
+     return 0;
+}
