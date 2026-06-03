588456989ac7dd58f76679b759fa1605a6e2fce2
Vansh Kumar <vanshkumar8810826073@gmail.com>
2026-06-03 21:14:38 +0530

    Third Program Done

---
+++ Day-5/Factors.c
@@ -0,0 +1,19 @@
+// Write a program to Print factors of a number
+#include <stdio.h>
+
+int main() {
+    int num, i;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factors of %d are: ", num);
+
+    for (i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
