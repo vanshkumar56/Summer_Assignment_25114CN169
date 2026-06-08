7cbd536f6389570f1adf3721c4aa881253fff638
Vansh Kumar <vanshkumar8810826073@gmail.com>
2026-06-08 14:37:46 +0530

    Second Program Done

---
+++ DAY-8/Pattern-1.c
@@ -0,0 +1,20 @@
+/*Write a program to Print number triangle.
+1
+12
+123
+1234
+12345 */
+#include <stdio.h>
+
+int main() {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
